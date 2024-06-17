28b4a741ecab40f34158020186e71cdcca07370f
Ebowwa <ec.arbee1@gmail.com>
2024-06-16 22:12:07 -0700

    Initial Commit

---
+++ Whispaw/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Whispaw
+//
+//  Created by ebowwa-mac-13inch on 6/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Whispaw/WhispawApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhispawApp.swift
+//  Whispaw
+//
+//  Created by ebowwa-mac-13inch on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct WhispawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
