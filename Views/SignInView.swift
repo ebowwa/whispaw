be44dceb3f9fb3296eaf2cb62c8b66cc0c4ee760
Ebowwa <ec.arbee1@gmail.com>
2024-06-16 23:09:31 -0700

    first commit

---
+++ Views/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  Whispaw
+//
+//  Created by ebowwa-mac-13inch on 6/16/24.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}
---
+++ Views/UserHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  UserHomeView.swift
+//  Whispaw
+//
+//  Created by ebowwa-mac-13inch on 6/16/24.
+//
+
+import SwiftUI
+
+struct UserHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserHomeView()
+}
