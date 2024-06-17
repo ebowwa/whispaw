dd4cabaac6e889668645b3ae6edca912558bd6f0
Ebowwa <ec.arbee1@gmail.com>
2024-06-16 23:16:14 -0700

    spelling correction

---
+++ Views/TutorialView.swift
@@ -0,0 +1,18 @@
+//
+//  TutorialView.swift
+//  Whispaw
+//
+//  Created by ebowwa-mac-13inch on 6/16/24.
+//
+
+import SwiftUI
+
+struct TutorialView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TutorialView()
+}
